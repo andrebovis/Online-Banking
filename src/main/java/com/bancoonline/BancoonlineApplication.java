1aa649598b4621f7fe8cf1debca0969d37443fcc
andre bovis <andre_bovis@hotmail.com>
2025-08-16 02:59:10 -0300

    feat: initial commit of bancoonline project

---
+++ src/main/java/com/bancoonline/BancoonlineApplication.java
@@ -0,0 +1,13 @@
+package com.bancoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancoonline/BancoonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
