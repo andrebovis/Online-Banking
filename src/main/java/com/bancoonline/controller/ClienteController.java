07bb7ed8eff725450edd39767b5b9a8254d38c32
andre bovis <andre_bovis@hotmail.com>
2025-08-18 03:45:38 -0300

    created clientcontroller e client repository

---
+++ src/main/java/com/bancoonline/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.bancoonline.controller;
+
+import com.bancoonline.model.Cliente;
+import com.bancoonline.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+        Cliente novoCliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(novoCliente);
+    }
+}
---
+++ src/main/java/com/bancoonline/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.bancoonline.repository;
+
+import com.bancoonline.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
